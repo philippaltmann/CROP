6e77d50ce6cf756a1e5b2f77a82f3c06c34d033d
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-12-20 19:06:08 +0100

    Added base algorithm

---
+++ algorithm/algorithm.py
@@ -0,0 +1,163 @@
+""" Generic Algorithm Class extending BaseAlgorithm with features needed by the training pipeline """
+import numpy as np; import pandas as pd
+import torch as th; import scipy.stats as st
+import random
+from stable_baselines3.common.base_class import BaseAlgorithm
+from stable_baselines3.common.callbacks import CallbackList
+from stable_baselines3.common.policies import ActorCriticPolicy, obs_as_tensor as obs
+from stable_baselines3.common.vec_env import VecEnv, VecNormalize
+from torch.utils.tensorboard.writer import SummaryWriter
+from typing import Any, Dict, List, Optional, Type, Union
+from tqdm import tqdm; import os
+import platform; import stable_baselines3 as sb3; import gym
+from .evaluation import EvaluationCallback
+
+class TrainableAlgorithm(BaseAlgorithm):
+  def __init__(self, envs:Optional[Dict[str,VecEnv]]=None, normalize:bool=False, policy:Union[str,Type[ActorCriticPolicy]]="MlpPolicy", path:Optional[str]=None, silent=False, **kwargs):
+    """ :param env: The environment to learn from (if registered in Gym, can be str)
+    :param policy: The policy model to use (MlpPolicy, CnnPolicy, ...) defaults to MlpPolicy
+    :param normalize: whether to use normalized observations, default: False
+    :param path: (str) the log location for tensorboard (if None, no logging) """
+    if envs: kwargs['env'] = envs['train']
+    self.envs, self.normalize, self.path, self.silent = envs, normalize, path, silent
+    self.eval_frequency, self.progress_bar, self._suffix = None, None, 'baseline'
+    super().__init__(policy=policy, verbose=0, **kwargs)
+    
+  def _setup_model(self) -> None:
+    if self.normalize: self.env = VecNormalize(self.env)
+    path = lambda seed: f"{self.path}/{seed}"
+
+    # path = lambda seed: f"{self.path}/{type(self).__name__}/{self._suffix}/{seed}"
+    gen_seed = lambda s=random.randint(0, 999): s if not os.path.isdir(path(s)) else gen_seed()
+    if self.seed is None: self.seed = gen_seed()
+    self.path = path(self.seed) if self.path is not None else None
+    self._naming = {'l': 'length-100', 'r': 'return-100', 's': 'safety-100'}; self._custom_scalars = {}
+    def eval_policy(s,a):
+      l = lambda t: t.cpu().detach().numpy()[0]
+      a,s = th.tensor([a]).to(self.device),obs(np.expand_dims(s, axis=0), self.device)
+      with th.no_grad(): values, log_prob, entropy = self.policy.evaluate_actions(s, a)
+      return np.exp(l(log_prob)),l(values)[0]
+    # self.heatmap_iterations = { 
+    #   'policy': (lambda _, s, a, r: eval_policy(s,a)[0], (0,1)),
+    #   # 'values': (lambda _, s, a, r: eval_policy(s,a)[1], (-100,50))
+    #   }
+    self.get_actions = lambda s: self.policy.get_distribution(obs(np.expand_dims(s, axis=0), self.device)).distribution.probs
+    self.heatmap_iterations = { 'policy': (lambda _, s, a, r: self.get_actions(s).cpu().detach().numpy()[0][a], (0,1)) }
+    super(TrainableAlgorithm, self)._setup_model()
+    self.writer, self._registered_ci = SummaryWriter(self.path) if self.path else None, [] #if self.path 
+    #and not self.silent
+    if not self.silent: print("+-------------------------------------------------------+\n"\
+      f"| System: {platform.platform()} |\n| Version: {platform.version()}  |\n" \
+      f"| GPU: {f'Enabled, version {th.version.cuda} on {th.cuda.get_device_name(0)}' if th.cuda.is_available() else'Disabled'}  |\n"\
+      f"| Python: {platform.python_version()} | PyTorch: {th.__version__} | Numpy: {np.__version__} |\n" \
+      f"| Stable-Baselines3: {sb3.__version__} | Gym: {gym.__version__} | Seed: {self.seed:3d}     |\n"\
+        "+-------------------------------------------------------+")
+
+  #Helper functions for writing model or hyperparameters
+  def _excluded_save_params(self) -> List[str]:
+    """ Returns the names of the parameters that should be excluded from being saved by pickling. 
+    E.g. replay buffers are skipped by default as they take up a lot of space.
+    PyTorch variables should be excluded with this so they can be stored with ``th.save``.
+    :return: List of parameters that should be excluded from being saved with pickle. """
+    return super(TrainableAlgorithm, self)._excluded_save_params() + ['get_actions', 'heatmap_iterations', '_naming', '_custom_scalars', '_registered_ci', 'envs', 'writer', 'progress_bar', 'silent']
+
+  def should_eval(self) -> bool: 
+    print(self.num_timesteps % self.eval_frequency)
+    return self.eval_frequency is not None and self.num_timesteps % self.eval_frequency == 0  
+
+  def learn(self, total_timesteps: int, stop_on_reward:float=None, eval_frequency=2048, **kwargs) -> "TrainableAlgorithm":
+    """ Learn a policy
+    :param total_timesteps: The total number of samples (env steps) to train on
+    :param stop_on_reward: Threshold of the mean 100 episode return to terminate training.
+    :param **kwargs: further aguments are passed to the parent classes 
+    :return: the trained model """
+    callback = EvaluationCallback(self, self.envs['test'], stop_on_reward=stop_on_reward); 
+    # if self.path: callback.evaluate() # Force Pre-training eval
+    if 'callback' in kwargs: callback = CallbackList([kwargs.pop('callback'), callback])    
+    alg = self.__class__.__name__; total = self.num_timesteps+total_timesteps; stepsize = self.n_steps * self.n_envs;
+    # if self.path is None: eval_frequency = None
+    if eval_frequency is not None: self.eval_frequency = eval_frequency * self.n_envs // stepsize * stepsize or eval_frequency * self.n_envs
+    hps = self.get_hparams(); hps.pop('seed'); hps.pop('num_timesteps');  
+    self.progress_bar = tqdm(total=total, unit="steps", postfix=[0,""], bar_format="{desc}[R: {postfix[0]:4.2f}][{bar}]({percentage:3.0f}%)[{n_fmt}/{total_fmt}@{rate_fmt}]") #desc=f"Training {alg}{hp}",
+    self.progress_bar.update(self.num_timesteps);
+    model = super(TrainableAlgorithm, self).learn(total_timesteps=total_timesteps, callback=callback, **kwargs)
+    self.progress_bar.close()
+    return model
+
+  def train(self, **kwargs) -> None:
+    # Update Progressbar 
+    self.progress_bar.postfix[0] = np.mean([ep_info["r"] for ep_info in self.ep_info_buffer])
+    if self.should_eval(): self.progress_bar.update(self.eval_frequency); #n_steps
+    summary, step = {}, self.num_timesteps 
+
+    super(TrainableAlgorithm, self).train(**kwargs) # Train PPO & Write Training Stats 
+    if self.writer == None or not self.should_eval(): return 
+
+    # Get infos from episodes & record rewards confidence intervals to summary 
+    epdata = {name: {ep['t']: ep[key] for ep in self.ep_info_buffer} for key,name in self._naming.items()}
+    termination_reasons = self.env.get_attr('termination_reasons')
+    [self.logger.record(str(r).replace('.','s/'), sum([env[r] for env in termination_reasons])) for r in termination_reasons[0]]
+    summary.update(self.prepare_ci(epdata, category="rewards", write_raw=True))
+
+    # Get Metrics from logger, record (float,int) as scalars, (ndarray) as confidence intervals
+    metrics = pd.json_normalize(self.logger.name_to_value, sep='/').to_dict(orient='records')[0]
+    summary.update({t: {step: v} for t,v in metrics.items() if isinstance(v, (float,int))})
+    summary.update(self.prepare_ci({t: {step: v} for t, v in metrics.items() if isinstance(v, np.ndarray)}))
+    
+    #Write metrcis summary to tensorboard 
+    [self.writer.add_scalar(tag, value, step) for tag,item in summary.items() for step,value in item.items()]
+    # [self.writer.add_histogram(key, values, step) for key, values in metrics.items() if isinstance(values, th.Tensor)]
+  
+  def prepare_ci(self, infos: dict, category=None, confidence:float=.95, write_raw=False) -> Dict: 
+    """ Computes the confidence interval := x(+/-)t*(s/√n) for a given survey of a data set. ref:
+    https://github.com/WangYueFt/rfs/blob/f8c837ba93c62dd0ac68a2f4019c619aa86b8421/eval/meta_eval.py#L19
+    :param infos: data dict in form {tag: {step: data, ...}}
+    :param categroy: category string to write summary (also custom scalar headline), if None: deduced from info tag
+    :param confidence: 
+    :param wite_raw: bool flag wether to write single datapoints
+    :return: Dict to be writen to tb in form: {tag: {step: value}} """
+    summary, s = {}, self.num_timesteps; 
+    factory = lambda c, t, m, ci: {f"{c}/{t}-mean":{s:m}, f"raw/{c}_{t}-lower":{s:m-ci}, f"raw/{c}_{t}-upper":{s:m+ci}}
+    if write_raw: summary.update({f"raw/{category}_{tag}": info for tag, info in infos.items()})
+    for tag, data in infos.items():
+      d = np.array(list(data.values())).flatten(); mean = d.mean(); ci = st.t.ppf((1+confidence)/2, len(d)-1) * st.sem(d)
+      if category == None: category,tag = tag.split('/')
+      c = self._custom_scalars.get(category, {}); t = c.get(tag); update = False
+      if not c: self._custom_scalars.update({category:{}}); c = self._custom_scalars.get(category); update = True
+      if not t: c.update({tag: ['Margin', list(factory(category, tag, 0, 0).keys())]}); update = True
+      if update: self.writer.add_custom_scalars(self._custom_scalars)
+      summary.update(factory(category, tag, mean, ci))
+    return summary
+
+  def get_hparams(self):
+    """ Fetches, filters & flattens own hyperparameters
+    :return: Dict of Hyperparameters containing numbers and strings only """ 
+    exclude = ['device','verbose','writer','tensorboard_log','start_time','rollout_buffer','eval_env']+\
+      ['policy','policy_kwargs','policy_class','lr_schedule','sde_sample_freq','clip_range','clip_range_vf']+\
+      ['env','observation_space','action_space','action_noise','ep_info_buffer','ep_success_buffer','target_kl']+\
+      ['envs', 'path', 'progress_bar', 'disc_kwargs', 'buffer']
+    hparams = pd.json_normalize(
+      {k: v.__name__ if isinstance(v, type) else v.get_hparams() if hasattr(v, 'get_hparams') else 
+          v for k,v in vars(self).items() if not(k in exclude or k.startswith('_'))
+      }, sep="_").to_dict(orient='records')[0]
+    hp_dis = {k:v for k,v in hparams.items() if isinstance(v, (int, bool))}
+    hp_num = {k:v for k,v in hparams.items() if isinstance(v, (float))}
+    hp_str = {k:str(v) for k,v in hparams.items() if isinstance(v, (str, list))}
+    hp_mdd = {k:v for k,v in hparams.items() if isinstance(v, (dict, th.Tensor))} #list
+    assert not len(hp_mdd), "Skipped writing hparams of multi-dimensional data"
+    return {**hp_dis, **hp_num, **hp_str, **{'env_name': self.env.get_attr('env_name')[0]}}
+  
+  def save(self, name="/model/train", **kwargs) -> None: 
+    kwargs['path'] = self.path + name; super(TrainableAlgorithm, self).save(**kwargs)
+
+  @classmethod
+  def load(cls, load, path, envs: Dict[str,VecEnv]=None, silent=False, **kwargs) -> "TrainableAlgorithm":
+    # TODO: get load path automated based on base path (gloablize path finding)
+    if envs: kwargs = {'env': envs['train'], 'envs': envs, 'silent': silent, **kwargs};
+    load = f"{load}/model/train"
+    assert os.path.exists(load+'.zip'), f"Attempting to load a model from {load} that does not exist"
+    model = super(TrainableAlgorithm, cls).load(load, **kwargs)
+    model.path = f"{path}/{type(model).__name__}/{model._suffix}/{model.seed}" if not silent else None
+    model.writer = SummaryWriter(log_dir=model.path) if model.path else None
+    model.num_timesteps -= model.num_timesteps%(model.n_steps * model.n_envs)
+    return model
---
+++ algorithm/evaluation.py
@@ -0,0 +1,65 @@
+import numpy as np; import torch as th; from typing import Any, Dict
+from stable_baselines3.common.base_class import BaseAlgorithm
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.evaluation import evaluate_policy
+from torch.utils.tensorboard.writer import SummaryWriter
+from algorithm.logging import write_hyperparameters
+
+class EvaluationCallback(BaseCallback):
+  """ Callback for evaluating an agent.
+  :param model: The model to be evaluated
+  :param eval_envs: A dict containing environments for testing the current model.
+  :param stop_on_reward: Whether to use early stopping. Defaults to True
+  :param reward_threshold: The reward threshold to stop at."""
+  def __init__(self, model: BaseAlgorithm, eval_envs: dict, stop_on_reward:float=None):
+    super(EvaluationCallback, self).__init__()
+    self.model = model; self.eval_envs = eval_envs; self.writer: SummaryWriter = self.model.writer
+    self.stop_on_reward, self.continue_training = stop_on_reward, True
+
+  def _on_rollout_end(self) -> None:
+    if self.writer == None: return 
+    mean_return = np.mean([ep_info["r"] for ep_info in self.model.ep_info_buffer])
+    # Uncomment for early stopping based on 100-mean training return
+    # if (self.stop_on_reward and mean_return >= self.stop_on_reward) or not self.continue_training: self.continue_training = False
+    if self.model.should_eval(): self.evaluate()
+
+  def _on_step(self) -> bool: 
+    """ Write timesteps to info & stop on reward threshold"""
+    [info['episode'].update({'t': self.model.num_timesteps}) for info in self.locals['infos'] if info.get('episode')]
+    return self.continue_training if self.stop_on_reward else True
+
+  def _on_training_end(self) -> None: # No Early Stopping->Unkown, not reached (continue=True)->Failure, reached (stopped)->Success
+    if self.writer == None: return 
+    status = 'STATUS_UNKNOWN' if not self.stop_on_reward else 'STATUS_FAILURE' if self.continue_training else 'STATUS_SUCCESS'
+    metrics = self.evaluate(); write_hyperparameters(self.model, list(metrics.keys()), status)
+
+  def evaluate(self):
+    """Run evaluation & write hyperparameters, results & video to tensorboard. Args:
+        write_hp: Bool flag to use basic method for writing hyperparams for current evaluation, defaults to False
+    Returns: metrics: A dict of evaluation metrics, can be used to write custom hparams """ 
+    step = self.model.num_timesteps
+    metrics = {k:v for label, env in self.eval_envs.items() for k, v in self.run_eval(env, label, step).items()}
+    [self.writer.add_scalar(key, value, step) for key, value in metrics.items()]; self.writer.flush()  
+    return metrics
+
+  def run_eval(self, env, label: str, step: int, eval_kwargs: Dict[str, Any]={}, write_video:bool=True):
+    video_buffer, FPS, metrics = [], 10, {} # Move video frames from buffer to tensor, unsqueeze & clear buffer
+    def retreive(buffer): entries = buffer.copy(); buffer.clear(); return th.tensor(np.array(entries)).unsqueeze(0)
+    record_video = lambda locals, _: video_buffer.append(locals['env'].render(mode='rgb_array'))
+    if env.envs[0].unwrapped.spec.nondeterministic: 
+      eval_kwargs.setdefault('n_eval_episodes', 100); eval_kwargs.setdefault('deterministic', False)
+    else:
+      eval_kwargs.setdefault('n_eval_episodes', 1); eval_kwargs.setdefault('deterministic', True)
+
+    r,_ = evaluate_policy(self.model, env, callback=record_video, **eval_kwargs)
+    metrics[f"metrics/{label}_reward"] = r
+    # print(f'{label} [det:{not env.envs[0].unwrapped.spec.nondeterministic}]: {metrics[f"metrics/{label}_reward"]}')
+    if (self.stop_on_reward and r >= self.stop_on_reward and label == 'validation') or not self.continue_training: 
+      # print('stop')
+      self.continue_training = False
+    # metrics[f'metrics/{label}_performance'] = env.env_method('get_performance')[0]
+    if write_video: self.writer.add_video(label, retreive(video_buffer), step, FPS) 
+    # Create & write tringle heatmap plots
+    [self.writer.add_figure(f'{k}_heatmap/{label}', env.envs[0].heatmap(*i), step) for k,i in self.model.heatmap_iterations.items()]  
+    self.writer.flush()
+    return metrics
