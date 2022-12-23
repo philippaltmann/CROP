f7fef83bc29cf7828671e0ffe2bc32ca294946d1
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2022-12-23 17:45:53 +0100

    Updated algorithm initialization

--- benchmark/__init__.py
+++ benchmark/__init__.py
@@ -1,5 +1,8 @@
-from .a2c import A2C
-from .dqn import DQN
-from .ppo import PPO
+# from .a2c import A2C
+# from .dqn import DQN
+from .ppo import PPO, PPO2
+from ai_safety_gym import FullyObservable
+# from ai_safety_gym import FO
+# TODO: SAC, RAD
 
-ALGS = ['A2C', 'DQN', 'PPO']
+ALGS = ['PPO', 'PPO2', 'FullyObservable']
