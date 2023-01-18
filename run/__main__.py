bf4b8e9883f80ededcc6a20b61f3b2b86391b7fc
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-01-18 03:21:50 +0100

    Updated runner

--- run/__main__.py
+++ run/__main__.py
@@ -1,4 +1,4 @@
-import setuptools; import argparse; import os; import random
+import setuptools; import argparse
 from ai_safety_gym import factory; 
 from algorithm import TrainableAlgorithm
 from benchmark import *; from crop import *
@@ -11,33 +11,32 @@ parser.add_argument( '--env', default='Train', metavar="Environment", help='The 
 parser.add_argument('-s', dest='seed', type=int, help='The random seed. If not specified a free seed [0;999] is randomly chosen')
 parser.add_argument('--load', type=str, help='Path to load the model.')
 parser.add_argument('--test', help='Run in test mode (dont write log files).', action='store_true')
+parser.add_argument('--stop', help='Stop at reward threshold.', action='store_true')
 parser.add_argument('--path', default='results', help='The base path, defaults to `results`')
-
-# Training Arguments
-parser.add_argument('-t', dest='timesteps', type=float, help='Number of timesteps to learn the model (eg 10e4)')
-parser.add_argument('-ts', dest='maxsteps', type=float, default=10e5, help='Maximum timesteps to learn the model (eg 10e4), using early stopping')
+parser.add_argument('-d',  dest='device', default='cuda', choices=['cuda','cpu'])
 
 # Get arguments 
 args = {key: value for key, value in vars(parser.parse_args()).items() if value is not None}; 
 
-method = args.pop('method'); method.append('FullyObservable') if len(method)<2 else None
-algorithm, wrapper = eval(method.pop(0)), eval(method[0])(*method[1:])
+method = args.pop('method'); algorithm = eval(method.pop(0))
+method = method or ['Observation', 'Full']; wrapper = eval(method[0])(*method[1:])
 
 name, spec = 'DistributionalShift', args.pop('env')
 envs, sparse = factory(name=name, spec=spec, wrapper=wrapper)
-path = None if args.pop('test') else f"{args.pop('path')}/{spec}/{algorithm.__name__ }/{' '.join(method)}"
+base = args.pop('path')
+path = None if args.pop('test') else f"{base}/{spec}/{algorithm.__name__ }/{' '.join(method[::-1])}"
 
 # Extract training parameters & merge model args
-stop_on_reward = envs['train'].get_attr('reward_threshold')[0] if 'maxsteps'in args else None
-timesteps = args.pop('timesteps', args.pop('maxsteps'))
-print(f"Training {algorithm.__name__ } {name} in {name} {spec}") 
-print(f"Stopping training at threshold {stop_on_reward}") if stop_on_reward else print(f"Training for {timesteps} steps")
+duration = lambda stop, spec: ([steps for key, steps in {
+  'stop': {'Train': 1e6, 'Maze': 1e6}, 'time': {'Train': 15e4, 'Maze7': 1e5, 'Mazes7': 2e5, 'Maze11': 15e4, 'Mazes11': 3e5}
+}['stop' if stop else 'time'].items() if key in spec][0], envs['train'].get_attr('reward_threshold')[0] if stop else False)
+timesteps, reward = duration(args.pop('stop'), spec)
+print(f"Training {algorithm.__name__ } {' '.join(method[::-1])} in {name} {spec} for {timesteps:.0f} steps {f'until {reward} is reached' if reward else ''}") 
 
 #Create, train & save model 
-args = {'envs': envs, 'path': path, **args} # 'seed': seed, # 'silent': path is None,
-load = args.pop('load', False); #load = base_path(seed,load) if load else False
-model:TrainableAlgorithm = algorithm.load(load=load, **args) if load else algorithm(**args) #device='cpu',
-
+args = {'envs': envs, 'path': path, **args}; load = args.pop('load', False);
+model:TrainableAlgorithm = algorithm.load(load=load, **args) if load else algorithm(**args)
 envs['train'].seed(model.seed); [env.seed(model.seed) for _,env in envs['test'].items()]
-model.learn(total_timesteps=timesteps, stop_on_reward=stop_on_reward, reset_num_timesteps = not load)
+model.learn(total_timesteps=timesteps, stop_on_reward=reward, reset_num_timesteps = not load)
 if path: model.save()
+print("Done")
