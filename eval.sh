2a063616d3d5b3dea361b491ff6a8254cb97f945
Philipp Altmann <philipp.altmann@ifi.lmu.de>
2023-01-18 03:26:36 +0100

    Updated evaluation runner

--- eval.sh
+++ eval.sh
@@ -1,6 +1,9 @@
-for RUN in 8; do # 1 2 3 4 5 6 7 8
-  for ALG in "PPO" "PPO CROP Action" "PPO CROP Object" "PPO CROP Radius"; do
-    echo "Running $ALG $RUN"
-    nohup python -m run $ALG --env Train --path results/1-evaluation &> "results/out/$ENV-$ALG-$RUN.out" &
+for RUN in 1 2 3 4 5 6 7 8 ; do 
+  for METHOD in "" "CROP Action" "CROP Object" "CROP Radius"; do
+    O="results/out-eval/PPO/$METHOD"; mkdir -p "$O"
+    nohup echo "Running PPO $METHOD $RUN" &> "$O/$RUN.out" &
+    nohup python -m run PPO $METHOD --stop --path results/1-evaluation &> "$O/$RUN.out" &
+    sleep 1s
   done
+  sleep 1h
 done
